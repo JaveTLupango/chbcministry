7b6411c424a4b09dfb724b162389e92a097ea42f
Jave <pangga.jave@gmail.com>
2021-03-24 22:37:48 +0800

    blogpost blade view update

---
+++ resources/views/partialview/blogpost/blogauthor.blade.php
@@ -0,0 +1,12 @@
+<div class="blog-author clearfix">
+    <img src="assets/img/blog-author.jpg" class="rounded-circle float-left" alt="">
+    <h4>Jane Smith</h4>
+    <div class="social-links">
+      <a href="https://twitters.com/#"><i class="icofont-twitter"></i></a>
+      <a href="https://facebook.com/#"><i class="icofont-facebook"></i></a>
+      <a href="https://instagram.com/#"><i class="icofont-instagram"></i></a>
+    </div>
+    <p>
+      Itaque quidem optio quia voluptatibus dolorem dolor. Modi eum sed possimus accusantium. Quas repellat voluptatem officia numquam sint aspernatur voluptas. Esse et accusantium ut unde voluptas.
+    </p>
+  </div><!-- End blog author bio -->
