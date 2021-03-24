f179742bd9a8b9d91af89d90c9435e3aff7c6d2a
Jave <pangga.jave@gmail.com>
2021-03-24 21:11:33 +0800

    topbar blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,24 +23,7 @@
 </head>
 
 <body>
-    <!-- ======= Top Bar ======= -->
-    <section id="topbar" class="d-none d-lg-block">
-        <div class="container d-flex">
-            <div class="contact-info mr-auto">
-                <ul>
-                    <li><i class="icofont-envelope"></i><a href="mailto:contact@example.com">floresdeb@gmail.com</a></li>
-                    <li><i class="icofont-phone"></i> +63 912 345 6789</li>
-                    <li><i class="icofont-clock-time icofont-flip-horizontal"></i> Mon-Fri 9am - 5pm</li>
-                </ul>
-            </div>
-            <div class="cta">
-                <a href="" class="scrollto"><i class="icofont-facebook"></i></a>
-                <a href="" class="scrollto"><i class="icofont-youtube"></i></a>
-                <a href="" class="scrollto"><i class="icofont-envelope"></i></a>
-                <a href="#about" class="scrollto">Get Started</a>
-            </div>
-        </div>
-    </section>
+ @include('partialview.topbar')
     <!-- ======= Header ======= -->
     <header id="header">
         <div class="container d-flex">
