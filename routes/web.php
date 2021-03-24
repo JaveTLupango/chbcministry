2baabc8e5217a140c95cdb617da90b900dd4466d
Jave <pangga.jave@gmail.com>
2021-03-24 20:55:16 +0800

    UI (User Interface)

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/blogpost', function () {
+    return view('post');
+});
