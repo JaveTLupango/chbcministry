f124c7eaf163d8ed338d9f6955861f05f7e063d9
Jave <pangga.jave@gmail.com>
2021-03-24 21:13:17 +0800

    header blade

---
+++ resources/views/partialview/header.blade.php
@@ -0,0 +1,26 @@
+<header id="header">
+        <div class="container d-flex">
+            <div class="logo mr-auto">
+                <!-- <h1 class="text-light"><a href="index.html"><span>Flexor</span></a></h1>
+                 Uncomment below if you prefer to use an image logo -->
+                <a href="/"><img src="assets/img/logo.jpg" alt="" class=""></a>
+            </div>
+            <nav class="nav-menu d-none d-lg-block" style="margin-top: 2%;">
+                <ul>
+                    <li class="active"><a href="index.html">Home</a></li>
+                    <li class="drop-down"><a href="">Oportunity</a>
+                        <ul>
+                            <li><a href="#">Member Ship Benefits</a></li>
+                            <li><a href="#">Business Program</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#team">Team</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#contact">Login</a></li>
+                </ul>
+            </nav>
+            <!-- .nav-menu -->
+        </div>
+    </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,31 +25,7 @@
 <body>
  @include('partialview.topbar')
     <!-- ======= Header ======= -->
-    <header id="header">
-        <div class="container d-flex">
-            <div class="logo mr-auto">
-                <!-- <h1 class="text-light"><a href="index.html"><span>Flexor</span></a></h1>
-                 Uncomment below if you prefer to use an image logo -->
-                <a href="index.html"><img src="assets/img/logo.jpg" alt="" class=""></a>
-            </div>
-            <nav class="nav-menu d-none d-lg-block" style="margin-top: 2%;">
-                <ul>
-                    <li class="active"><a href="index.html">Home</a></li>
-                    <li class="drop-down"><a href="">Oportunity</a>
-                        <ul>
-                            <li><a href="#">Member Ship Benefits</a></li>
-                            <li><a href="#">Business Program</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#team">Team</a></li>
-                    <li><a href="blog.html">Blog</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </nav>
-            <!-- .nav-menu -->
-        </div>
-    </header>
+@include('partialview.header')
     <!-- End Header -->
     <!-- ======= Hero Section ======= -->
     <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
