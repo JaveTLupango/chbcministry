392be767c7c43e1ff23e59234ed18f99c0f05677
Jave <pangga.jave@gmail.com>
2021-03-25 16:24:19 +0800

    core blade view update

--- resources/views/partialview/core.blade.php
+++ resources/views/partialview/core.blade.php
@@ -10,7 +10,12 @@
                 <div class="card" style="background-image: url(assets/img/values-1.jpg);">
                     <div class="card-body">
                         <h5 class="card-title"><a href="">Our Mission</a></h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua.</p>
+                        <p class="card-text">Bringing <b>individual person and families</b> into the saving intimate relationship with <b>Jesus Christ</b>, 
+                            transforming them to be committed Members of His body, 
+                            develop them into Christ life maturity, 
+                            and equip them for His intentional Ministry 
+                            in the church and to have a lifestyle Mission 
+                            in the world to Magnify His name.</p>
                         <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
                     </div>
                 </div>
@@ -18,8 +23,25 @@
             <div class="col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="100">
                 <div class="card" style="background-image: url(assets/img/values-2.jpg);">
                     <div class="card-body">
-                        <h5 class="card-title"><a href="">Our Plan</a></h5>
-                        <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque laudantium, totam rem.</p>
+                        <h5 class="card-title"><a href="">Core Value</a></h5>
+                        <ul>
+                            <li>
+                                <p class="card-text">Leader : The Valor to shape a better future....</p>
+                            </li>
+                            <li>
+                                <p class="card-text">Integrity : be a genuine, reliable and fruitfu....</p>
+                            </li>
+                            <li>
+                                <p class="card-text">Excellence : Stand out and be responsible p....</p>
+                            </li>
+                            <li>
+                                <p class="card-text">Passion : Committed in heartand mind to in....</p>
+                            </li>
+                            <li>
+                                <p class="card-text">Quality : Revolutionary and effective in the i....</p>
+                            </li>
+                        </ul>
+                        
                         <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
                     </div>
                 </div>
@@ -28,7 +50,7 @@
                 <div class="card" style="background-image: url(assets/img/values-3.jpg);">
                     <div class="card-body">
                         <h5 class="card-title"><a href="">Our Vision</a></h5>
-                        <p class="card-text">Nemo enim ipsam voluptatem quia voluptas sit aut odit aut fugit, sed quia magni dolores.</p>
+                        <p class="card-text"><b>Community Hope baptist Chruch</b> exist to be intentional disciple making body of Jesus Christ.</p>
                         <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
                     </div>
                 </div>
@@ -36,8 +58,12 @@
             <div class="col-md-6 d-flex align-items-stretch mt-4" data-aos="fade-up" data-aos-delay="300">
                 <div class="card" style="background-image: url(assets/img/values-4.jpg);">
                     <div class="card-body">
-                        <h5 class="card-title"><a href="">Our Care</a></h5>
-                        <p class="card-text">Nostrum eum sed et autem dolorum perspiciatis. Magni porro quisquam laudantium voluptatem.</p>
+                        <h5 class="card-title"><a href="">Our Plan</a></h5>
+                            <div class="text-center">
+                                <button type="button" class="btn btn-primary" title="OPPORTUNITY">OPPORTUNITY</button>
+                                <button type="button" class="btn btn-secondary" title="Spiritual Foundation Discipleship Making" >S . F . D . M</button>
+                                <button type="button" class="btn btn-success" title="Strategies In Discipleship" >S . I . D</button>
+                            </div>                                
                         <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
                     </div>
                 </div>
