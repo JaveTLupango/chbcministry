df00c1361de6afa8e58e836d066bb26414fb287e
Jave <pangga.jave@gmail.com>
2021-03-28 13:00:08 +0800

    header menu login

--- resources/views/partialview/header.blade.php
+++ resources/views/partialview/header.blade.php
@@ -19,7 +19,7 @@
                     <li><a href="#team">Team</a></li>
                     <li><a href="/blog">Blog</a></li>
                     <li><a href="#contact">Contact</a></li>
-                    <li><a href="/">Login</a></li>
+                    <li><a href="/login">Login</a></li>
                 </ul>
             </nav>
             <!-- .nav-menu -->
