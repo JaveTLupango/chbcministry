d03be278448d707a80de8051bf5ef33b91494595
Jave <pangga.jave@gmail.com>
2021-03-29 09:54:50 +0800

    app service and user migration table update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,14 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('contact');
+            $table->string('status');
+            $table->string('usertype');
+            $table->string('bday');
+            $table->LongBlob('profile');
             $table->rememberToken();
             $table->timestamps();
         });
