0b15207f81c4fad74e0cdb80dca01cb7a79f87f8
Jave <pangga.jave@gmail.com>
2021-03-29 11:04:29 +0800

    homepage update

--- resources/views/partialview/homepage.blade.php
+++ resources/views/partialview/homepage.blade.php
@@ -1,7 +1,7 @@
 <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
     <div class="container" data-aos="fade-in">
-        <h1>Welcome to CHBC Ministry</h1>
-        <h2>We invite you to joined our online webinar bible ministry.</h2>
+    <h1>{{ $s_info[1]['info2'] }}</h1>
+        <h2>{{ $s_info[1]['infobody'] }}<h2>
         <div class="d-flex align-items-center">
             <i class="bx bxs-right-arrow-alt get-started-icon"></i>
             <a href="#about" class="btn-get-started scrollto">Get Started</a>
