5a3b4e5ec7a658401532360d46d3080468eac1d0
Jave <pangga.jave@gmail.com>
2021-03-29 10:31:58 +0800

    controller of system info

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/home', function () {
+    return view('home');
+});
 Route::get('/blog', function () {
     return view('blog');
 });
