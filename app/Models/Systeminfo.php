d7761a9518b00f7b04d0224580cc31079c9a614f
Jave <pangga.jave@gmail.com>
2021-03-29 09:42:20 +0800

    system info model create

---
+++ app/Models/Systeminfo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Systeminfo extends Model
+{
+    use HasFactory;
+}
