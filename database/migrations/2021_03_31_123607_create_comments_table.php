2b861db4e4f8dfef06ef20c20c7af65ee65c2998
Jave <pangga.jave@gmail.com>
2021-03-31 20:44:15 +0800

    update model and database

--- database/migrations/2021_03_31_123607_create_comments_table.php
+++ database/migrations/2021_03_31_123607_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBlogsTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,15 @@ class CreateBlogsTable extends Migration
      */
     public function up()
     {
-        Schema::create('blogs', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('blogtitle');
-            $table->longText('blogbody');
             $table->string('userid');
-            $table->integer('like_id');
-            $table->integer('comment_id');
-            $table->integer('type');
+            $table->string('blogid');
+            $table->mediumText('commentbody');
+            $table->datetime('dtcomment');
             $table->integer('status');
-            $table->integer('category');
-            $table->dateTime('dtposted');          
+            $table->integer('type');
+            $table->integer('reply_at');
             $table->timestamps();
         });
     }
@@ -35,6 +33,6 @@ class CreateBlogsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('blogs');
+        Schema::dropIfExists('comments');
     }
 }
